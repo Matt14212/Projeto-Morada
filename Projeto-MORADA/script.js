d4231b4305346d76c66470cc25d2f5bbf0076e9b
waberbaggins <waberxbernardino@gmail.com>
2024-06-19 16:19:12 -0300

    cadastro-imóvel
    Formulário para cadastrar imóvel

--- Projeto-MORADA/script.js
+++ Projeto-MORADA/script.js
@@ -1,28 +1,40 @@
 window.addEventListener('load', function() {
-    const form = document.getElementById('cadastroForm');
+    const form = document.getElementById('imovelForm');
     const cadastrarBtn = document.getElementById('cadastrarBtn');
     const cancelarBtn = document.getElementById('cancelarBtn');
     const resultadoDiv = document.getElementById('resultado');
 
     cadastrarBtn.addEventListener('click', function() {
         if (form.checkValidity()) {
-            const nome= document.getElementById('nome').value;
-            const telefone = document.getElementById('telefone').value;
-            const email = document.getElementById('email').value;
-            const usuario = document.getElementById('usuario').value;
-            const senha = document.getElementById('senha').value;
-            const data = document.getElementById('data').value;
+            const emailC= document.getElementById('emailC').value;
+            const telC = document.getElementById('telC').value;
+            const opcao = document.getElementById('opcao').value;
+            const tipo = document.getElementById('tipo').value;
+            const metros = document.getElementById('metros').value;
+            const quartos = document.getElementById('quartos').value;
+            const banheiros = document.getElementById('banheiros').value;
+            const salas = document.getElementById('salas').value;
+            const endereco = document.getElementById('endereco').value;
+            const preco = document.getElementById('preco').value;
+            const adicionais = document.getElementById('adicionais').value;
 
             resultadoDiv.innerHTML = `
                 <div class="resultado">
-                <h2>Cadastro realizado com sucesso!</h2>
-                <h3>Seus dados:</h3>
-                <p><strong>Nome:</strong> ${nome}</p>
-                <p><strong>Telefone:</strong> ${telefone}</p>
-                <p><strong>Email:</strong> ${email}</p>
-                <p><strong>Data de nascimento:</strong> ${data}</p>
-                <p><strong>Usuário:</strong> ${usuario}</p>
-                <p><strong>Senha:</strong> ${senha}</p>
+                <h2>Imóvel cadastrado com sucesso</h2>
+                <h3>Dados:</h3>
+                <p><strong>Email de contato:</strong> ${emailC}</p>
+                <p><strong>Telefone de contato:</strong> ${telC}</p>
+                <p><strong>Tipo de negociação:</strong> ${opcao}</p>
+                <p><strong>Tipo do imóvel:</strong> ${tipo}</p>
+                <p><strong>Email de contato:</strong> ${emailC}</p>
+                <h3>Dados do imóvel:</h3>
+                <p><strong>Área:</strong> ${metros}m²</p>
+                <p><strong>Quartos:</strong> ${quartos}</p>
+                <p><strong>Banheiros:</strong> ${banheiros}</p>
+                <p><strong>Salas:</strong> ${salas}</p>
+                <p><strong>Endereço:</strong> ${endereco}</p>
+                <p><strong>Valor:</strong> R$${preco}</p>
+                <h3><strong>Informações adicionais:</strong> ${adicionais} </h3>
                 </div>
             `;
         } else {
